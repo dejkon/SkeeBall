d2da9c6e038051a8f7a2ec142da8926023e47d20
Al Cuppernell-Nolan <dejkon@gmail.com>
2017-12-13 16:48:33 -0500

    working Score Board

--- SkeeBallBasic.py
+++ SkeeBallBasic.py
@@ -14,27 +14,21 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED   = (255, 0, 0)
 
-# draw the black background onto the surface
-windowSurface.fill(BLACK)
+SCORE = 000
+BALLS = 0
 
-# draw stuffs to screen
-digitalFont = pygame.font.Font("digital.ttf", 350) # font argument needs full path if not in this folder
-
-score = digitalFont.render("999", 1, GREEN)
-balls = digitalFont.render("9", 1, RED)
-
-centeredTopBottom = windowSurface.get_height() // 2 - score.get_height() // 2
-
-# (x,y)
-windowSurface.blit(score, (20, centeredTopBottom))
-# windowSurface.blit(balls, (20 + 3 * (width // 3) + 10 + 10 + 40, centeredTopBottom))
-windowSurface.blit(balls, (windowSurface.get_width() - balls.get_width() - 20, centeredTopBottom))
-
-# draw the window onto the screen
-pygame.display.update()
+def updateGame(keyScore):
+    global BALLS, SCORE
+    if (BALLS < 9):
+        BALLS += 1
+        SCORE += keyScore
+    elif (BALLS == 9):
+        BALLS = 0
+        SCORE = 000
 
 # run the game loop
 while True:
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -47,10 +41,26 @@ while True:
                 sys.exit()
 
             if event.key == pygame.K_1:
-                pygame.quit()
-                sys.exit()
+                updateGame(10)
+
             # if event.key == pygame.K_RIGHT:
             #
             # if event.key == pygame.K_UP:
             #
             # if event.key == pygame.K_DOWN:
+
+    # definitions
+    digitalFont = pygame.font.Font("digital.ttf", 350) # font argument needs full path if not in this folder
+    score = digitalFont.render(str(SCORE), 1, GREEN)
+    balls = digitalFont.render(str(BALLS), 1, RED)
+    centeredTopBottom = windowSurface.get_height() // 2 - score.get_height() // 2
+
+    # draw the black background onto the surface
+    windowSurface.fill(BLACK)
+
+    # (x,y)
+    windowSurface.blit(score, (20, centeredTopBottom))
+    windowSurface.blit(balls, (windowSurface.get_width() - balls.get_width() - 20, centeredTopBottom))
+
+    # draw the window onto the screen
+    pygame.display.update()
