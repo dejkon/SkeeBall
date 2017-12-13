3d01f2fd83a96971642df96cdffdaab75c26c0b4
Al Cuppernell-Nolan <dejkon@gmail.com>
2017-12-13 14:03:17 -0500

    deletes

--- SkeeBallBasic.py
+++ SkeeBallBasic.py
@@ -14,22 +14,12 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED   = (255, 0, 0)
 
-# score = 000
-# balls = 0
-
-#def add_Score(digit, rank):
-#    if rank == rHundreds:
-#        if digit != 9:
-#    return str("digit")
-
 # draw the black background onto the surface
 windowSurface.fill(BLACK)
 
 # draw stuffs to screen
 digitalFont = pygame.font.Font("digital.ttf", 350) # font argument needs full path if not in this folder
 
-# hundreds = digitalFont.render(str("9"), 1, GREEN)
-# tens = digitalFont.render("9", 1, GREEN)
 ones = digitalFont.render("999", 1, GREEN)
 balls = digitalFont.render("9", 1, RED)
 
@@ -39,9 +29,6 @@ centeredTopBottom = windowSurface.get_height() // 2 - height // 2
 
 # (x,y)
 windowSurface.blit(ones, (20, centeredTopBottom))
-# windowSurface.blit(hundreds, (20, windowHeight))
-# windowSurface.blit(tens, (20 + width + 10, windowHeight))
-# windowSurface.blit(ones, (20 + 2 * width + 10 + 10, windowHeight))
 windowSurface.blit(balls, (20 + 3 * (width // 3) + 10 + 10 + 40, centeredTopBottom))
 
 # draw the window onto the screen
