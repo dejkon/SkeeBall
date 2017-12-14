aaa4c8f333ede92d257d1b348afacb09fff62fbc
Al Cuppernell-Nolan <dejkon@gmail.com>
2017-12-13 19:41:16 -0500

    font correction

--- SkeeBallBasic.py
+++ SkeeBallBasic.py
@@ -1,15 +1,14 @@
 import pygame, sys
 from pygame.locals import *
 
-# set up pygame
+# set up pygame and window -----------------------------------------------------
 pygame.init()
 pygame.font.init()
-
-# set up the window
 windowSurface = pygame.display.set_mode((800, 480), 0, 32)
 pygame.display.set_caption('Skee Ball')
+#-------------------------------------------------------------------------------
 
-# set up the colors (red, green, blue)
+# variable defs ----------------------------------------------------------------
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED   = (255, 0, 0)
@@ -17,6 +16,7 @@ RED   = (255, 0, 0)
 SCORE = 000
 BALLS = 0
 
+# function defs
 def updateGame(keyScore):
     global BALLS, SCORE
     if (BALLS < 9):
@@ -25,8 +25,9 @@ def updateGame(keyScore):
     elif (BALLS == 9):
         BALLS = 0
         SCORE = 000
+#-------------------------------------------------------------------------------
 
-# run the game loop
+# run the game loop ------------------------------------------------------------
 while True:
 
     for event in pygame.event.get():
@@ -43,24 +44,39 @@ while True:
             if event.key == pygame.K_1:
                 updateGame(10)
 
-            # if event.key == pygame.K_RIGHT:
-            #
-            # if event.key == pygame.K_UP:
-            #
-            # if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_2:
+                updateGame(20)
 
-    # definitions
-    digitalFont = pygame.font.Font("digital.ttf", 350) # font argument needs full path if not in this folder
-    score = digitalFont.render(str(SCORE), 1, GREEN)
+            if event.key == pygame.K_3:
+                updateGame(30)
+
+            if event.key == pygame.K_4:
+                updateGame(40)
+
+            if event.key == pygame.K_5:
+                updateGame(50)
+
+            if event.key == pygame.K_9:
+                updateGame(100)
+
+            if event.key == pygame.K_0:
+                updateGame(0)
+
+    # definitions # font argument needs full path if not in this folder
+    digitalFont = pygame.font.Font("digital.ttf", 350)
+    score = digitalFont.render(str(SCORE).zfill(3), 1, GREEN)
     balls = digitalFont.render(str(BALLS), 1, RED)
-    centeredTopBottom = windowSurface.get_height() // 2 - score.get_height() // 2
+    centeredTopBottom = windowSurface.get_height() // 2 \
+                        - score.get_height() // 2
 
     # draw the black background onto the surface
     windowSurface.fill(BLACK)
 
     # (x,y)
     windowSurface.blit(score, (20, centeredTopBottom))
-    windowSurface.blit(balls, (windowSurface.get_width() - balls.get_width() - 20, centeredTopBottom))
+    windowSurface.blit(balls, (windowSurface.get_width() \
+                        - balls.get_width() - 20, centeredTopBottom))
 
     # draw the window onto the screen
     pygame.display.update()
+#-------------------------------------------------------------------------------
